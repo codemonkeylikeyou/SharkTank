31a4decbb22113a96a5158f7c3a43583af466df8
triggedbytrig <36166142+triggedbytrig@users.noreply.github.com>
2018-02-12 11:45:16 -0500

    Battleship.java (comments from Carlos)

--- src/me/zrosfjord/bs/Battleship.java
+++ src/me/zrosfjord/bs/Battleship.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 public class Battleship implements Runnable {
 	
+	// initializing objects and array lists
 	public static int BOARD_SIZE = 5;
 	private List<Entity> entities;
 	
@@ -16,16 +17,19 @@ public class Battleship implements Runnable {
 	
 	
 	public Battleship() {
+		// places entities object on the map
 		entities = new ArrayList<Entity>();
 		
 		entities.add(new Human(4, 4, false));
 		entities.add(new Shark(0, 3));
 	}
 	
-	public void start() {
+	public void start() { // starts program
+		// exits out of start() if running = true
 		if(running)
 			return;
 		
+		// starts a new thread meaning that the game will start
 		running = true;
 		thread = new Thread(this);
 		thread.start();
@@ -34,13 +38,14 @@ public class Battleship implements Runnable {
 	public void run() {
 		printBoard();
 		
+		// loop will continue to update the board until the program ends
 		while(running) {
 			update();
 			printBoard();
 		}
 	}
 	
-	
+	// updates position of each object in entities
 	private void update() {
 		for(int x = 0; x < entities.size(); x++) {
 			Entity e = entities.get(x);
@@ -51,14 +56,20 @@ public class Battleship implements Runnable {
 	
 	public void printBoard() {
 		Map<Point, Entity> points = new HashMap<Point, Entity>();
+		
+		// loop associates each entity has a meaningful value on the map
+		// according to it's position
 		for(Entity e : entities) {
 			points.put(new Point(e.getX(), e.getY()), e);
 		}
 		
+		// prints out each board
 		for(int x = 0; x < BOARD_SIZE; x++) {
 			for(int y = 0; y < BOARD_SIZE; y++) {
 				Point p = new Point(y, x);
 				
+				// for each entity the loop encounters, it will replace the entitie's
+				// value with an icon (h or s) at the object's current position
 				if(points.containsKey(p)) {
 					Entity ent = points.get(p);
 					System.out.print(ent.getIcon() + "\t");
@@ -69,11 +80,11 @@ public class Battleship implements Runnable {
 			
 			System.out.println();
 		}
-		
+		// separates after each printed board update
 		System.out.println("-----------------------------------");
 	}
 	
-	
+	// starts the game
 	public static void main(String[] args) {
 		new Battleship().start();
 	}
